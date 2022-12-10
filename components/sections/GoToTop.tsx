f0993cb380951291b2792372cb6270977ad7c5d1
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-12-10 19:34:05 +0530

    FIXED: minor UI issue

--- components/sections/GoToTop.tsx
+++ components/sections/GoToTop.tsx
@@ -1,13 +1,13 @@
 export default function GoToTop() {
     return (
-        <a href="#">
+        <a href="#" className="z-40">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="2.5"
             stroke="#835B38"
-            className="w-12 h-12 block bottom-10 right-10 fixed bg-brown4/50 rounded-full"
+            className="w-12 h-12 block bottom-10 right-14 fixed bg-brown4/50 rounded-full"
           >
             <path
               stroke-linecap="round"
