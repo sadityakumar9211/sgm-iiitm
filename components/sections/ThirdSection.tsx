a14dc3bfa5e95fa824c411a867d8cb0410883f82
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-12-10 18:05:42 +0530

    FIX: deployment errors

--- components/sections/ThirdSection.tsx
+++ components/sections/ThirdSection.tsx
@@ -11,7 +11,7 @@ export default function ThirdSection() {
               <div className="bg-[url('/childrens_day.jpeg')] w-full md:h-60 h-56 bg-center bg-cover">
                 <div className="hover:bg-sky-600/90 transition-all duration-500 w-full h-full bg-sky-500/50 z-20 text-white">
                   <div className="pt-40 md:pt-44 pl-8 w-full h-full hover:md:pt-6 hover:pt-6 transition-all duration-300">
-                    <div className="md:text-2xl text-xl">Children's Day</div>
+                    <div className="md:text-2xl text-xl">Children&apos;s Day</div>
                     <div className="md:text-xl text-lg">14th November</div>
                     <div className="text-montserrat text-sm w-4/5 text-justify md:pt-4 pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labo.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
                   </div>
@@ -22,7 +22,7 @@ export default function ThirdSection() {
               <div className="bg-[url('/teachers_day.jpeg')] w-full md:h-60 h-56 bg-center bg-cover">
                 <div className="hover:bg-amber-600/90 transition-all duration-500 w-full h-full bg-amber-500/50 z-20 text-white">
                   <div className="pt-40 md:pt-44 pl-8 w-full h-full hover:md:pt-6 hover:pt-6 transition-all duration-300">
-                    <div className="md:text-2xl text-xl">Teacher's Day</div>
+                    <div className="md:text-2xl text-xl">Teacher&apos;s Day</div>
                     <div className="md:text-xl text-lg">5th September</div>
                     <div className="text-montserrat text-sm w-4/5 text-justify md:pt-4 pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labo.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
                   </div>
