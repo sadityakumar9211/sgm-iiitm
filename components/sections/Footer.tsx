414c34d511ca8b3f5f50a11b5f341f7a30aff517
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-12-10 19:40:16 +0530

    MODIFY: IIITM site now opens in new tab

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex flex-row pl-12 space-x-8">
-          <a href="https://iiitm.ac.in">
+          <a href="https://iiitm.ac.in" target="_blank">
             <img
               src="/institute_logo.svg"
               alt="IIITM Logo"
