279d863c77d5dd528ad500fb451896b2bb21c32d
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-12-10 09:30:02 +0530

    ADD: full screen footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -2,47 +2,164 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col text-center bg-brown4 py-12 px-2">
-      <div className="text-xl font-montserrat">We are Social :&#41;</div>
-      <div className="flex flex-row space-x-4 mx-auto">
-        <div>
-          <img src="/facebook.png" alt="Facebook Icon" className="w-10 h-10" />
+    <footer className="flex flex-col text-center bg-brown4 py-12 mt-12 px-2">
+      {/* big footer starts here */}
+      <div className="md:flex flex-col hidden">
+        <div className="flex flex-row justify-around pb-16">
+          <div>
+            <div className="text-left">General</div>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left"
+            >
+              Donate
+            </a>
+          </div>
+          <div>
+            <div className="text-left">Explore</div>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              Our Mission
+            </a>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              Events
+            </a>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              Captures
+            </a>
+            <a className="font-montserrat text-md text-brown3 text-left block">
+              Our Team
+            </a>
+          </div>
+          <div>
+            <div className="text-left">Contact Us</div>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              sgm@iiitm.ac.in
+            </a>
+            <a
+              href="#"
+              className="font-montserrat text-md text-brown3 text-left block"
+            >
+              sgm.iiitm.ac.in
+            </a>
+          </div>
+          <div>
+            <div className="text-left">We are Social :&#41;</div>
+            <div>
+              <div className="flex flex-row space-x-4 mx-auto">
+                <div>
+                  <img
+                    src="/facebook.png"
+                    alt="Facebook Icon"
+                    className="w-10 h-10"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="/instagram.png"
+                    alt="Instagram Icon"
+                    className="w-10 h-10"
+                  />
+                </div>
+                <div>
+                  <img
+                    src="/youtube.png"
+                    alt="Youtube Icon"
+                    className="w-10 h-10"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <img
-            src="/instagram.png"
-            alt="Instagram Icon"
-            className="w-10 h-10"
-          />
-        </div>
-        <div>
-          <img src="/youtube.png" alt="Youtube Icon" className="w-10 h-10" />
+        <div className="flex flex-row pl-6 space-x-8">
+          <div>
+            <img
+              src="/institute_logo.png"
+              alt="IIITM Logo"
+              className="w-16 h-20"
+            />
+          </div>
+          <div>
+            <img src="/logo.svg" alt="SGM Logo" className="w-20 h-20" />
+          </div>
+          <div className="text-left pl-12 items-center font-montserrat text-md">
+            <p>Student Gyan Movement</p>
+            <p>
+              ABV-Indian Institute of Information Technology and Management
+              Gwalior,
+            </p>
+            <p>Morena Link Road, Gwalior, Madhya Pradesh, India, 474015.</p>
+          </div>
         </div>
       </div>
-      <div className="text-lg font-montserrat py-2 w-full">
-        mail us at: sgm@iiitm.ac.in
-      </div>
-      <div className="flex flex-row space-x-4 mx-auto py-2">
-        <div>
-          <img
-            src="/institute_logo.png"
-            alt="Institute Logo"
-            className="w-10 h-12"
-          />
+      {/* big footer ends here */}
+      
+      {/* mobile footer starts here */}
+      <div className="md:hidden">
+        <div className="text-xl font-montserrat">We are Social :&#41;</div>
+        <div className="flex flex-row space-x-4 mx-auto">
+          <div>
+            <img
+              src="/facebook.png"
+              alt="Facebook Icon"
+              className="w-10 h-10"
+            />
+          </div>
+          <div>
+            <img
+              src="/instagram.png"
+              alt="Instagram Icon"
+              className="w-10 h-10"
+            />
+          </div>
+          <div>
+            <img src="/youtube.png" alt="Youtube Icon" className="w-10 h-10" />
+          </div>
         </div>
-        <div>
-          <img
-            src="/logo.svg"
-            alt="SGM Logo"
-            className="w-12 h-12"
-          />
+        <div className="text-lg font-montserrat py-2 w-full">
+          mail us at: sgm@iiitm.ac.in
+        </div>
+        <div className="flex flex-row space-x-4 mx-auto py-2">
+          <div>
+            <img
+              src="/institute_logo.png"
+              alt="Institute Logo"
+              className="w-10 h-12"
+            />
+          </div>
+          <div>
+            <img src="/logo.svg" alt="SGM Logo" className="w-12 h-12" />
+          </div>
+        </div>
+        <div className="md:text-md font-montserrat text-yellow-900 w-full md:w-4/5">
+          Student Gyan Movement
+        </div>
+        <div className="md:text-md font-montserrat text-yellow-900 w-full md:11/12">
+          ABV-Indian Institute of Information Technology and Management Gwalior,{" "}
+        </div>
+        <div className="md:text-md font-montserrat text-yellow-900 w-full md:w-4/5">
+          Morena Link Road, Gwalior, Madhya Pradesh, India, 474015.
         </div>
       </div>
-      <div className="md:text-md font-montserrat text-yellow-900 w-full md:w-4/5">Student Gyan Movement</div>
-      <div className="md:text-md font-montserrat text-yellow-900 w-full md:11/12">
-        ABV-Indian Institute of Information Technology and Management Gwalior,{" "}
-      </div>
-      <div className="md:text-md font-montserrat text-yellow-900 w-full md:w-4/5">Morena Link Road, Gwalior, Madhya Pradesh, India, 474015.</div>
+      {/* mobile footer ends here*/}
     </footer>
   );
 }
